9ff21da878f7922d6ae0fd30f697f1a0ce9754b3
Romain Commandé <commande.romain@gmail.com>
2015-10-16 20:08:50 +0200

    Mapper first implementation

--- elmapper/elmapper/apps/mapper/models.py
+++ elmapper/elmapper/apps/mapper/models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import csv
+import itertools
+import logging
 
 from django.db import models
 
@@ -11,6 +13,20 @@ PRODUCT_FIELDS = [
     ('category', 'category'),
     ('color', 'color'),
 ]
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_field_by_name(model_class, field_name):
+    """Return specific field for given model class"""
+    return [field for field in model_class._meta.fields if field.name == field_name][0]
+
+
+def get_mapping_modelclass(model_class):
+    """Return the mapping config class for given model class"""
+    return [klass for klass in CONFIG_CLASSES
+            if get_field_by_name(klass, "internal").related_model == model_class.related_model][0]
 
 
 class Brand(models.Model):
@@ -68,6 +84,33 @@ class ImportedProductCSV(models.Model):
     csv_file = models.FileField()
     upload_date = models.DateTimeField(auto_now_add=True)
 
+    def save(self):
+        known_columns = FieldMappingConfig.objects.values_list('product_field', flat=True)
+        csv_reader = csv.reader(self.csv_file, delimiter=",", quotechar="\"")
+        headers = csv_reader.next()
+
+        # Determine readable fields
+        fields = [field for field in headers if field in known_columns]
+
+        # Read rows
+        for row in csv_reader:
+            mapped_fields = {key: value for key, value in itertools.izip(headers, row)
+                             if key in fields}
+            result_fields = {}
+
+            for field_name, field_value in mapped_fields.items():
+                field_class = [field for field in Product._meta.fields if field.name == field_name][0]
+                if isinstance(field_class, models.ForeignKey):
+                    mapping_class = get_mapping_modelclass(field_class)
+                    result_fields[field_name] = mapping_class.objects.get(original_fk=field_value).internal
+                else:
+                    result_fields[field_name] = field_value
+
+            #product = Product(**{key: value for key, value in itertools.izip(headers, row)})
+            product = Product(**result_fields)
+            logging.info(str(product))
+            product.save()
+
 
 class FieldMappingConfig(models.Model):
     """A mapping between an external and an internal field name"""
@@ -110,3 +153,6 @@ class ColorConfig(MappingConfig):
 class BrandConfig(MappingConfig):
     """A mapping between an external and an internal brand foreign keys"""
     internal = models.ForeignKey(Brand)
+
+
+CONFIG_CLASSES = [CategoryConfig, ColorConfig, BrandConfig]
--- elmapper/elmapper/apps/mapper/views.py
+++ elmapper/elmapper/apps/mapper/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 
 def test_view(request):
     return HttpResponse("It´ works !")
+
+
+def mapping_generator(imported_product_csv):
+    """Return a generator who yield mapping csv data line by line"""
+    pass
