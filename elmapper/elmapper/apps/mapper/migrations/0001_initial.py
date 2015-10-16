0fc84e712c04b6693f73f1e97c8f012ed90927ae
Romain Commandé <commande.romain@gmail.com>
2015-10-16 12:04:17 +0200

    Start a naive basic model

---
+++ elmapper/elmapper/apps/mapper/migrations/0001_initial.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Brand',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250)),
+                ('description', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('label', models.CharField(max_length=250)),
+                ('description', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Color',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=250)),
+                ('description', models.TextField()),
+                ('price', models.DecimalField(max_digits=19, decimal_places=2)),
+                ('brand', models.ForeignKey(to='mapper.Brand')),
+                ('category', models.ForeignKey(to='mapper.Category')),
+                ('color', models.ForeignKey(to='mapper.Color')),
+            ],
+        ),
+    ]
--- elmapper/elmapper/apps/mapper/models.py
+++ elmapper/elmapper/apps/mapper/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+
+
+class Category(models.Model):
+    label = models.CharField(max_length=250)
+    description = models.TextField()
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=19, decimal_places=2)
+    brand = models.ForeignKey(Brand)
+    category = models.ForeignKey(Category)
+    color = models.ForeignKey(Color)
