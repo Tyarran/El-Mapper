4bf8196535f498b5d75c9b67059a083e5d6580c0
Romain Commandé <commande.romain@gmail.com>
2015-10-20 23:00:23 +0200

    Add Colander schema for mapping config

---
+++ elmapper/elmapper/apps/mapper/schemas.py
@@ -0,0 +1,24 @@
+import colander
+
+
+class FKMapping(colander.MappingSchema):
+    """A Foreign key Mapping between a external and internal id"""
+    external_fk = colander.SchemaNode(colander.String())
+    internal_fk = colander.SchemaNode(colander.String())
+
+
+class FKMappings(colander.SequenceSchema):
+    """Collection of foreign keys mapping"""
+    fkmapping = FKMapping()
+
+
+class Field(colander.MappingSchema):
+    """A field represented by our external/internal name and a foreign keys mapping"""
+    external_name = colander.SchemaNode(colander.String())
+    model_name = colander.SchemaNode(colander.String())
+    foreign_key_mapping = FKMappings()
+
+
+class Fields(colander.SequenceSchema):
+    """A collection of fields"""
+    field = Field()
